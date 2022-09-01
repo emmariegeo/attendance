a38fc7337af7ee26aba90e5a7fb2e167b2923667
emmariegeo <57061691+emmariegeo@users.noreply.github.com>
2022-09-01 16:49:21 -0700

    Update conn.php

--- db/conn.php
+++ db/conn.php
@@ -1,8 +1,16 @@
 <?php
-    $host = '127.0.0.1';
-    $db = 'attendance_db';
-    $user = 'root';
-    $pass = '';
+    // Development Connection
+    // $host = '127.0.0.1';
+    // $db = 'attendance_db';
+    // $user = 'root';
+    // $pass = '';
+    // $charset = 'utf8mb4';
+
+    // Remote Database Connection
+    $host = getenv('DB_URL');
+    $db = getenv('DB_NAME');
+    $user = getenv('DB_USER');
+    $pass = getenv('DB_PASS');
     $charset = 'utf8mb4';
     // Connector String 
     $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
